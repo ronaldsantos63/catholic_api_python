3cef33704b69583ce7bc7100ccacaa198c57fc89
Ronald Santos <ronaldsantos63@gmail.com>
2024-06-07 15:07:27 -0300

    Ajuste do arquivo passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,8 @@
-import importlib.util
-import os
 import sys
+import os
 
-
+# Adiciona o caminho da aplicação ao sys.path
 sys.path.insert(0, os.path.dirname(__file__))
 
-spec = importlib.util.spec_from_file_location("wsgi", "app.py")
-wsgi = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(wsgi)
-
-application = wsgi.app
+# Importa a aplicação Flask
+from app import app as application  # Supondo que sua aplicação esteja no arquivo app.py
